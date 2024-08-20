53cec3c47f91878895dbe382db5b13af74e495b7
Shameer Mukhtar <94970687+ShameerMukhtar@users.noreply.github.com>
2024-08-21 03:22:40 +0500

    create home page

---
+++ Myntra-Clone/myntra-react/src/Components/Bag.jsx
@@ -0,0 +1,20 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Bag=()=>{
+  return<>
+  <Header></Header>
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container">
+        </div>
+        <div className="bag-summary">
+        </div>
+
+      </div>
+    </main>
+    <Footer></Footer>
+  
+  </>
+}
+export default Bag;
